1e45ace6af9cae87db7da7224a14fa612936845d
Badr Almadhi <badr.almadhi95@gmail.com>
2024-04-28 23:00:56 -0600

    tyring to figure out the signUp

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -12,7 +12,7 @@ export const QUERY_LOGIN = gql`
   }
 `;
 
-export const ADD_USE = gql`
+export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
